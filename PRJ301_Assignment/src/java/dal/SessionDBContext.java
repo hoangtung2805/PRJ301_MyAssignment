faaede19777e26082ed4eb278d549ff0f3eb9200
Phan Hoang Tung <103381423+hoangtung2805@users.noreply.github.com>
2022-10-22 15:15:43 +0700

    Update SessionDBContext.java
    add update attandance for teacher

--- PRJ301_Assignment/src/java/dal/SessionDBContext.java
+++ PRJ301_Assignment/src/java/dal/SessionDBContext.java
@@ -10,10 +10,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Attandance;
 import model.Group;
 import model.Lecturer;
 import model.Room;
 import model.Session;
+import model.Student;
 import model.Subject;
 import model.TimeSlot;
 
@@ -30,7 +32,55 @@ public class SessionDBContext extends DBContext<Session>{
 
     @Override
     public void update(Session model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            connection.setAutoCommit(false);
+            String sql = "UPDATE [Session] SET attanded = 1 WHERE sesid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, model.getId());
+            stm.executeUpdate();
+
+            //remove old attandances
+            sql = "DELETE Attandance WHERE sesid = ?";
+            PreparedStatement stm_delete = connection.prepareStatement(sql);
+            stm_delete.setInt(1, model.getId());
+            stm_delete.executeUpdate();
+
+            //insert new attandances
+            for (Attandance att : model.getAtts()) {
+                sql = "INSERT INTO [Attandance]\n"
+                        + "           ([sesid]\n"
+                        + "           ,[stdid]\n"
+                        + "           ,[present]\n"
+                        + "           ,[description]\n"
+                        + "           ,[record_time])\n"
+                        + "     VALUES\n"
+                        + "           (?\n"
+                        + "           ,?\n"
+                        + "           ,?\n"
+                        + "           ,?\n"
+                        + "           ,GETDATE())";
+                PreparedStatement stm_insert = connection.prepareStatement(sql);
+                stm_insert.setInt(1, model.getId());
+                stm_insert.setInt(2, att.getStudent().getId());
+                stm_insert.setBoolean(3, att.isPresent());
+                stm_insert.setString(4, att.getDescription());
+                stm_insert.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ex) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @Override
@@ -92,7 +142,20 @@ public class SessionDBContext extends DBContext<Session>{
                     sub.setName(rs.getString("subname"));
                     g.setSubject(sub);
                     
+                    ses.setDate(rs.getDate("date"));
+                    ses.setIndex(rs.getInt("index"));
+                    ses.setAttanded(rs.getBoolean("attanded"));
                 }
+                Student s = new Student();
+                s.setId(rs.getInt("stdid"));
+                s.setName(rs.getString("stdname"));
+                Attandance a = new Attandance();
+                a.setStudent(s);
+                a.setSession(ses);
+                a.setPresent(rs.getBoolean("present"));
+                a.setDescription(rs.getString("description"));
+                ses.getAtts().add(a);
+                
             
         }
         } catch (SQLException ex) {
